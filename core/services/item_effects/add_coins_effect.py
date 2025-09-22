75b4f0e7573e2a81ed203bf633baa0a719bc8df3
Forrest Chui <ame9807@gmail.com>
2025-09-22 09:19:26 +0900

    Revert "Refactor restructure command handlers"

--- core/services/item_effects/add_coins_effect.py
+++ core/services/item_effects/add_coins_effect.py
@@ -35,8 +35,4 @@ class AddCoinsEffect(AbstractItemEffect):
         user.coins += total_amount
         self.user_repo.update(user)
 
-        return {
-            "success": True,
-            "message": f"💰 打开 {item_template.name} x{quantity}，获得了 {total_amount} 金币！",
-            "coins_gained": total_amount,
-        }
+        return {"success": True, "message": f"💰 打开 {item_template.name} x{quantity}，获得了 {total_amount} 金币！"}
