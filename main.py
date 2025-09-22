5a7a589b0f7e2f274aff8bfa87cfeaba448fc811
XXNM <ame9807@outlook.com>
2025-09-22 13:18:23 +0900

    refactor: Update alias for open all money bags command

--- main.py
+++ main.py
@@ -291,7 +291,7 @@ class FishingPlugin(Star):
         async for r in inventory_handlers.use_item(self, event):
             yield r
 
-    @filter.command("开启全部钱袋", alias={"开全部钱袋", "全部钱袋"})
+    @filter.command("开启全部钱袋", alias={"打开全部钱袋"})
     async def open_all_money_bags(self, event: AstrMessageEvent):
         async for r in inventory_handlers.open_all_money_bags(self, event):
             yield r
